53e1a9df900f4eeb05d5d60649626aa699e579f4
rishirishhh <rishikavya19@gmail.com>
2024-11-15 00:55:47 +0530

    feat: added logout functionality and a session middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,26 @@
-export default function Home() {}
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
+  return (
+    <div>
+      Only visible to authorised users
+      <Button onClick={() => mutate}>Logout</Button>
+    </div>
+  );
+}
